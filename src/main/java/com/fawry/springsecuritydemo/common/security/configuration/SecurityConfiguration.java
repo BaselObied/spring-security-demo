70342ae46dcf66eac0a25756092f68fc51fbadbf
basel.obied <basel.obied@fawry.com>
2026-06-28 08:52:05 +0300

    secure unsing servlet

--- src/main/java/com/fawry/springsecuritydemo/common/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/fawry/springsecuritydemo/common/security/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,6 +25,7 @@ public class SecurityConfiguration {
         return httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/users/**").hasAuthority("ROLE_ADMIN")
+//                        .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(apiKeyAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
